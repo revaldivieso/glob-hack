baefdaf7247a966299f3a82b76ff8ce1f565a573
Katherine Muñoz <56646813+katsmunoz@users.noreply.github.com>
2020-05-30 13:03:25 -0400

    adding Router, bootstrap, welcome.js, login.js and navigation bar (#2)
    * agregando router y carpeta Components
    
    * agregando bootstrap, router, login.js y welcome.js
    
    * agregando ruta de bootstrap en app.js
    
    * agregando navigation bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Components/Home';
 import Login from './Components/Login';
 import Register from './Components/Register';
 import { AuthProvider } from './Components/Auth';
+import { Welcome } from './Components/Welcome';
 
 function App() {
   return (
     <AuthProvider>
       <Router>
         <div>
+          <Route exact path="/welcome" component={Welcome} />
           <Route path='/login' component={Login} />
           <Route path='/register' component={Register} />
           <Route exact path='/' component={Home} />
